fbf605f3b2dd5eb794210bb63c1772354fef7409
Deepak Garg <90500037+Deepakgarg2309@users.noreply.github.com>
2023-08-19 19:52:36 +0530

    Update JsonReadingApplication.java

--- src/main/java/com/example/demo/JsonReadingApplication.java
+++ src/main/java/com/example/demo/JsonReadingApplication.java
@@ -2,20 +2,32 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
+
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class JsonReadingApplication {
-	public static void main(String[] args) throws InterruptedException {
-        // Create a rate limiter with a capacity of 15 tokens and a refill interval of 4 seconds
-        RateLimiter rateLimiter = new RateLimiter(15, 4000);
+    public static void main(String[] args) throws InterruptedException {
+        RateLimiterManager rateLimiterManager = new RateLimiterManager();
+
+        String user1 = "user123";
+        String user2 = "user456";
 
         for (int i = 0; i < 20; i++) {
-            if (rateLimiter.makeApiCall()) {
-                System.out.println("API call " + (i + 1) + " success");
+            if (rateLimiterManager.makeApiCall(user1)) {
+                System.out.println("User 1 - API call " + (i + 1) + " success");
             } else {
-                System.out.println("API call " + (i + 1) + " rate limited");
+                System.out.println("User 1 - API call " + (i + 1) + " rate limited");
             }
+
+            if (rateLimiterManager.makeApiCall(user2)) {
+                System.out.println("User 2 - API call " + (i + 1) + " success");
+            } else {
+                System.out.println("User 2 - API call " + (i + 1) + " rate limited");
+            }
+
             Thread.sleep(3000); // Simulate some processing time before the next API call
         }
     }
-
 }
