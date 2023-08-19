7a7172396f8c800c14df9dbbe82e965ce0ca45b2
Deepak Garg <90500037+Deepakgarg2309@users.noreply.github.com>
2023-08-19 19:51:41 +0530

    Update JsonReadingApplicationTests.java

--- src/test/java/com/example/demo/JsonReadingApplicationTests.java
+++ src/test/java/com/example/demo/JsonReadingApplicationTests.java
@@ -3,11 +3,31 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class JsonReadingApplicationTests {
+	private RateLimiterManager rateLimiterManager;
 
-	@Test
-	void contextLoads() {
+	@BeforeEach
+	public void setUp() {
+		rateLimiterManager = new RateLimiterManager();
 	}
 
+	@Test
+	public void testRateLimiterManager() {
+		String userId = "user1";
+		String userId2 = "user2";
+		int apiCallCount = 8;
+
+		for (int i = 0; i < apiCallCount; i++) {
+			assertTrue(rateLimiterManager.makeApiCall(userId));
+			assertTrue(rateLimiterManager.makeApiCall(userId2));
+
+		}
+
+		assertFalse(rateLimiterManager.makeApiCall(userId));
+	}
 }
